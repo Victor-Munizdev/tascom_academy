f38241cedbb276f6bba860d76cc1ca375672ca8d
Victor Muniz <munizzvr@gmail.com>
2025-06-07 14:52:21 -0300

    Commit

---
+++ src/react/src/index.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+function Relogio() {
+  const [hora, setHora] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setHora(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(intervalo);
+  }, []);
+
+  return <h1>{hora}</h1>;
+}
+
+export default Relogio;
