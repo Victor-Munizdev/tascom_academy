9933520708e2f4cc8a484b6b57ebc34133158b8c
Victor Muniz <munizzvr@gmail.com>
2025-08-02 14:34:03 -0300

    Delete Projeto Final directory

--- Projeto Final/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom"
-
-export default function Header() {
-  return (
-    <nav>
-      <Link to="/">Início</Link> | <Link to="/create">Novo Portfólio</Link>
-    </nav>
-  )
-}
